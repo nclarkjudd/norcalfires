fc535b28a29e7f60dec2910f3ab0110fa398b7af
Nick Judd <nick@nclarkjudd.com>
2017-10-10 17:25:52 -0500

    map

--- getmap.sh
+++ getmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget https://fsapps.nwcg.gov/afm/data_viirs_iband/fireptdata/viirs_iband_fire_last7_2017_282_conus_shapefile.zip
-unzip -o viirs_iband_fire_last7_2017_282_conus_shapefile.zip -d shp 
+unzip viirs_iband_fire_last7_2017_282_conus_shapefile.zip -d shp 
 ogr2ogr -sql "SELECT * FROM viirs_iband_fire_last7_2017_282_conus WHERE LAT < 42 AND LONG < -120 AND LONG > -124 AND LAT > 36" -f GeoJSON -t_srs crs:84 www/geojson/viirs_iband_fire_last7_2017_282_conus.geojson shp/viirs_iband_fire_last7_2017_282_conus.shp -mapFieldType Date|DATE=String
