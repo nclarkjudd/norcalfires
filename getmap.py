0c8c61e174be58f24e901eb3a45b68d05969820f
Nick Judd <nick@nclarkjudd.com>
2017-10-11 12:18:00 -0500

    for use with postgis

--- getmap.py
+++ getmap.py
@@ -3,12 +3,13 @@ import psycopg2
 import osgeo.ogr
 import requests
 from zipfile import ZipFile
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import BytesIO
 
 now = datetime.today()
 day_of_year = datetime.strftime(now, "%j")
 year = datetime.strftime(now, "%Y")
+last_24hrs = datetime.today() - timedelta(hours=24)
 table = 'viirs_iband_fire_last7_' + year + '_' + day_of_year + '_conus'
 url =  'https://fsapps.nwcg.gov/afm/data_viirs_iband/fireptdata/' + table + '_shapefile.zip'
 shapefile = 'shp/' + table + '.shp'
@@ -43,8 +44,32 @@ def insert_rows(raw_shapefile):
         cursor.execute("INSERT INTO fires VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ST_GeometryFromText(%s, 4269))",
                     (bt4temp, conf, date_time, fire, fire_id, frp, julian, lat, long, sat_src, src, spix, tpix, wkt))
     conn.commit()
+    conn.close()
+
+def get_activity(from_after):
+    conn = psycopg2.connect("dbname=fires")
+    cursor = conn.cursor()
+    makeview_str = "CREATE OR REPLACE VIEW active_fire_buffers AS"\
+                   " SELECT ST_Buffer(ST_Transform(geom, 3311),187.5,'quad_segs=1') AS new_geom,"\
+                   " bt4temp, conf, date_time, frp FROM firedata"\
+                   " WHERE date_time > %s;"
+    makeview_data = (from_after,)
+    cursor.execute(makeview_str, makeview_data)
+    conn.commit()
+    get_geojson = "SELECT row_to_json(fc)"\
+                  "FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features"\
+                  "FROM (SELECT 'Feature' As type, "\
+                  "ST_AsGeoJSON(afb.new_geom)::json As geometry, "\
+                  "row_to_json((SELECT l FROM (SELECT bt4temp, conf, date_time, frp) AS l "\
+                  ")) As properties "\
+                  "FROM active_fire_buffers As afb   ) As f )  As fc;"
+    cursor.execute(get_geojson)
+    geojson = cursor.fetchall()
+    with open('www/geojson/latest_fire_buffers.geojson','w') as outf:
+        json.dump(geojson[0],outf)
 
 
 if __name__ == '__main__':
     get_and_unzip()
     insert_rows(shapefile)
+    get_activity(last_24hrs)
